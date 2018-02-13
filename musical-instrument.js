380078230c684937321ab67e1125fab04f94c0bf
ayau3 <35642979+ayau3@users.noreply.github.com>
2018-02-12 18:42:58 -0800

    Update musical-instrument.js

--- musical-instrument.js
+++ musical-instrument.js
@@ -10,13 +10,16 @@ var xPos = 100/2;
 var yPos = 100;
 var diameter = 5;
 
+var playingA = false;
+var playingS = false;
+var playingD = false;
+var playingF = false;
+
 function setup() {
   backgroundColor = color(255, 0, 255);
   textAlign(CENTER);
   freq = random(100,300);
   playing = false
-
-  
 
   
   osc = new p5.Oscillator();
@@ -40,7 +43,7 @@ function draw() {
   if (colorCounter3 >= 255) {
     colorCounter3 = 0;
   }
-  background(51,255,51);
+  background(255,255,255);
   ellipse(mouseX,mouseY,10)
   
   // we should probably remove the text
@@ -65,6 +68,26 @@ function draw() {
     	}
     }
   }
+   if (playingA) {
+    background(0, 0, 255, 25);
+    fill(random(100, 255), 100, 100);
+    ellipse(random(width), random(height), 10);
+   }
+   else if (playingS) {
+    background(0, 255, 0, 25);
+    fill(100, random(50, 255), 100);
+    ellipse(random(width), random(height), 10);
+   }
+  else if (playingD) {
+    background(255, 0, 0, 25);
+    fill(10, random(100, 250), 255);
+    ellipse(random(width), random(height), 10);
+   }
+  else if (playingF) {
+    background(10, 255, 200, 25);
+    fill(random(1,100), 100, 100);
+    ellipse(random(width), random(height), 10);
+   }
 }
 
 //Changed this so that the frequency is based on the location of the mouse
@@ -73,15 +96,19 @@ function keyPressed() {
     print("got key press for ", key);
     if (key == 'A') {
       freq = mouseX + mouseY + 174;
+      playingA = true;
     }
     if (key == 'S') {
      freq = mouseX + mouseY+196;
+      playingS = true;
     }
     if (key == 'D') {
       freq = mouseX + mouseY+220;
+      playingD = true;
     }
     if (key == 'F') {
       freq = mouseX + mouseY+246;
+      playingF = true;
     }
     osc.freq(freq);
     osc.amp(0.5, 0.1);
@@ -94,14 +121,18 @@ function keyReleased() {
   if (key == 'A') {
     osc.amp(0.0, 0.5);
     playing = false;
+    playingA = false;
   } else if (key == 'S') {
     osc.amp(0.0, 0.5);
     playing = false;
+    playingS = false;
   } else if (key == 'D') {
     osc.amp(0.0, 0.5);
     playing = false;
+    playingD = false;
   } else if (key == 'F') {
     osc.amp(0.0, 0.5);
     playing = false;
+    playingF = false;
   }
 }

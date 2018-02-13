ce0fb3919d5920fc94bf95c161cfefb23495d951
coletriebold <35664168+coletriebold@users.noreply.github.com>
2018-02-13 03:04:43 -0800

    Update musical-instrument.js

--- musical-instrument.js
+++ musical-instrument.js
@@ -10,11 +10,6 @@ var xPos = 100/2;
 var yPos = 100;
 var diameter = 5;
 
-var playingA = false;
-var playingS = false;
-var playingD = false;
-var playingF = false;
-
 function setup() {
   backgroundColor = color(255, 0, 255);
   textAlign(CENTER);
@@ -22,8 +17,10 @@ function setup() {
   playing = false
 
   
+
+  
   osc = new p5.Oscillator();
-  osc.setType('triangle');
+  osc.setType('saw');
   osc.freq(freq);
   osc.amp(0);
   osc.start();
@@ -43,11 +40,13 @@ function draw() {
   if (colorCounter3 >= 255) {
     colorCounter3 = 0;
   }
-  background(255,255,255);
+  background(51,255,51);
   ellipse(mouseX,mouseY,10)
   
   // we should probably remove the text
-  text('click here,\nthen press A, S,\n D, or F to play', width / 2, 40);
+  text('click here,\nthen press keys\n and move mouse \nto play', width / 2, 30);
+  text('Low',15, 10);
+  text('High',width-15, height-5);
   
   // pressing the mouse while playing creates bubbles
   if (playing) {
@@ -68,48 +67,24 @@ function draw() {
     	}
     }
   }
-   if (playingA) {
-    background(0, 0, 255, 25);
-    fill(random(100, 255), 100, 100);
-    ellipse(random(width), random(height), 10);
-   }
-   else if (playingS) {
-    background(0, 255, 0, 25);
-    fill(100, random(50, 255), 100);
-    ellipse(random(width), random(height), 10);
-   }
-  else if (playingD) {
-    background(255, 0, 0, 25);
-    fill(10, random(100, 250), 255);
-    ellipse(random(width), random(height), 10);
-   }
-  else if (playingF) {
-    background(10, 255, 200, 25);
-    fill(random(1,100), 100, 100);
-    ellipse(random(width), random(height), 10);
-   }
 }
 
-//Changed this so that the frequency is based on the location of the mouse
 function keyPressed() {
   if (keyIsPressed){
     print("got key press for ", key);
-    if (key == 'A') {
-      freq = mouseX + mouseY + 174;
-      playingA = true;
+    if (key == 'W') {
+      osc.setType('square');
     }
-    if (key == 'S') {
-     freq = mouseX + mouseY+196;
-      playingS = true;
+    else if (key == 'Q') {
+      osc.setType('sine');
     }
-    if (key == 'D') {
-      freq = mouseX + mouseY+220;
-      playingD = true;
+    else if (key == 'E') {
+      osc.setType('sawtooth');
     }
-    if (key == 'F') {
-      freq = mouseX + mouseY+246;
-      playingF = true;
+    else if (key == 'R') {
+      osc.setType('triangle');
     }
+    freq = mouseX+mouseY+100;
     osc.freq(freq);
     osc.amp(0.5, 0.1);
     playing = true;
@@ -121,18 +96,14 @@ function keyReleased() {
   if (key == 'A') {
     osc.amp(0.0, 0.5);
     playing = false;
-    playingA = false;
   } else if (key == 'S') {
     osc.amp(0.0, 0.5);
     playing = false;
-    playingS = false;
   } else if (key == 'D') {
     osc.amp(0.0, 0.5);
     playing = false;
-    playingD = false;
   } else if (key == 'F') {
     osc.amp(0.0, 0.5);
     playing = false;
-    playingF = false;
   }
 }
